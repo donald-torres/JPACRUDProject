a1bafd90b2c75198ee0f3e386c908b0165f18a7b
Donald Torres <dctorres5597@gmail.com>
2023-12-01 17:40:37 -0700

    Added entities and JUnit Test

---
+++ JPAFavoriteMovies/src/main/java/com/skilldistillery/jpafavoritemovies/entities/Movie.java
@@ -0,0 +1,101 @@
+package com.skilldistillery.jpafavoritemovies.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Movie {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String description;
+	@Column(name = "release_year")
+	private int releaseYear;
+	@Column(name = "image_url")
+	private String imageUrl;
+	private int length;
+	
+	public Movie() 	{}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public int getLength() {
+		return length;
+	}
+
+	public void setLength(int length) {
+		this.length = length;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, imageUrl, length, releaseYear, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		return Objects.equals(description, other.description) && id == other.id
+				&& Objects.equals(imageUrl, other.imageUrl) && length == other.length
+				&& releaseYear == other.releaseYear && Objects.equals(title, other.title);
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", title=" + title + ", description=" + description + ", releaseYear=" + releaseYear
+				+ ", imageUrl=" + imageUrl + ", length=" + length + "]";
+	}
+	
+	
+	
+}
---
+++ JPAFavoriteMovies/src/test/java/com/skilldistillery/jpafavoritemovies/entities/MovieTest.java
@@ -0,0 +1,53 @@
+package com.skilldistillery.jpafavoritemovies.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class MovieTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Movie movie;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAFavoriteMovies");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		movie = em.find(Movie.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		movie = null;
+	}
+
+	
+	@Test
+	void test_Movie_basic_mappings() {
+		assertNotNull(movie);
+		assertEquals("Princess Mononoke", movie.getTitle());
+//		assertEquals(1993, movie.getReleaseYear());
+//		assertEquals(0.99, movie.getRentalRate());
+//		assertEquals(20.99, movie.getReplacementCost());
+	}
+
+}

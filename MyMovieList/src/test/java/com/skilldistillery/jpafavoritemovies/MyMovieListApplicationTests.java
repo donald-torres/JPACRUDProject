8cb8f424ba0fcacbb7c48f6a19af7c4938d1a066
Donald Torres <dctorres5597@gmail.com>
2023-12-01 17:52:34 -0700

    linked MCV and JPA

---
+++ MyMovieList/src/test/java/com/skilldistillery/jpafavoritemovies/MyMovieListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpafavoritemovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMovieListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
